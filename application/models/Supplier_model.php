47fd4875419c19ba15707eb55ed911891252cdd7
Alex Putra <alex19ti@mahasiswa.pcr.ac.id>
2022-12-22 10:00:37 +0700

    fix

--- application/models/Supplier_model.php
+++ application/models/Supplier_model.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+
 use GuzzleHttp\Client;
+
 class Supplier_model extends CI_Model
 {
     public $table = 'supplier';
@@ -9,75 +11,73 @@ class Supplier_model extends CI_Model
     public function __construct()
     {
         parent::__construct();
-        $this->_client = new Client([
-            'base_uri' => 'http://localhost/rest-api-alex-project/Supplier',
-            'auth' => ['admin', '1234', 'alex' => 'alex']
-        ]);
+        // $this->_client = new Client([
+        //     'auth' => ['admin', '1234', 'alex' => 'alex']
+        // ]);
     }
 
     public function get()
     {
-        // $this->db->select('j.*, d.nama as nama_kajur');
-        // $this->db->from('jurusan j');
-        // $this->db->join('dosen d', 'j.nama_kajur = d.id');
-        // $query = $this->db->get();
-        // return $query->result_array();
-        $response = $this->_client->request('GET', 'supplier',[
-            'query' => [
-                'BPF-TI' => 'bpfalex'
-            ]
-        ]);
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $result['data'];
+        $this->db->select('*');
+        $this->db->from('supplier');
+        $query = $this->db->get();
+        return $query->result_array();
+        // $response = $this->_client->request('GET', 'supplier', [
+        //     'query' => [
+        //         'BPF-TI' => 'bpfalex'
+        //     ]
+        // ]);
+        // $result = json_decode($response->getBody()->getContents(), true);
+        // return $result['data'];
     }
     public function getById($id)
     {
-        // $this->db->from($this->table);
-        // $this->db->where('id', $id);
-        // $query=$this->db->get();
-        // return $query->row_array();
-        $response = $this->_client->request('GET', 'supplier',[
-            'query' => [
-                'id' => $id,
-                'BPF-TI' => 'bpfalex'
-            ]
-        ]);
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $result['data'][0];
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+        return $query->row_array();
+        // $response = $this->_client->request('GET', 'supplier', [
+        //     'query' => [
+        //         'id' => $id,
+        //         'BPF-TI' => 'bpfalex'
+        //     ]
+        // ]);
+        // $result = json_decode($response->getBody()->getContents(), true);
+        // return $result['data'][0];
     }
     public function update($where, $data)
     {
-        // $this->db->update($this->table, $data, $where);
-        // return $this->db->affected_rows();
-        $response = $this->_client->request('PUT', 'supplier', [
-            'form_params' => $data
-        ]);
-        $result = json_decode($response->getBody()->getContents(), true);
-        return $result;
+        $this->db->update($this->table, $data, $where);
+        return $this->db->affected_rows();
+        // $response = $this->_client->request('PUT', 'supplier', [
+        //     'form_params' => $data
+        // ]);
+        // $result = json_decode($response->getBody()->getContents(), true);
+        // return $result;
     }
     public function insert($data)
     {
-        // $this->db->insert($this->table, $data);
-        // return $this->db->insert_id();
-        $response = $this->_client->request('POST', 'supplier', [
-            'form_params' => $data
-        ]);
-        $result = json_decode($response->getBody()->getContents(), true);
-        return $result;
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+        // $response = $this->_client->request('POST', 'supplier', [
+        //     'form_params' => $data
+        // ]);
+        // $result = json_decode($response->getBody()->getContents(), true);
+        // return $result;
     }
     public function delete($id)
     {
-        // $this->db->where($this->id, $id);
-        // $this->db->delete($this->table);
-        // return $this->db->affected_rows();
-        $response = $this->_client->request('DELETE', 'supplier',[
-            'form_params' => [
-                'id' => $id,
-                'BPF-TI' => 'bpfalex'
-            ]
-        ]);
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $result['data'];
+        $this->db->where($this->id, $id);
+        $this->db->delete($this->table);
+        return $this->db->affected_rows();
+        // $response = $this->_client->request('DELETE', 'supplier', [
+        //     'form_params' => [
+        //         'id' => $id,
+        //         'BPF-TI' => 'bpfalex'
+        //     ]
+        // ]);
+        // $result = json_decode($response->getBody()->getContents(), true);
+        // return $result['data'];
     }
 
     public function tacc()
